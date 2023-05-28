1f74e7f4fb10d6db19fd4d492fe431567aeea278
qerdcv <vadym.tishchenko2@gmail.com>
2023-05-28 20:18:24 +0300

    Add debug logs

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -22,5 +22,6 @@ func (u *User) Validate() error {
 }
 
 func (u *User) HashPassword(secret []byte) {
+	fmt.Println(string(secret), "<-- secret")
 	u.Password = fmt.Sprintf("%x", string(pbkdf2.Key([]byte(u.Password), secret, 100_000, 32, sha256.New)))
 }
